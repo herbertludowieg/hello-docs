ca07ce23f97435cd909c3475c6d287a58fc0b2ea
Herbert Ludowieg <herbertl@buffalo.edu>
2019-10-29 10:45:44 -0400

    Adding some simple code

---
+++ hello-docs/hello.py
@@ -0,0 +1,15 @@
+
+
+class SimpleClass:
+    def __init__(self):
+        '''This is a simple class that does precisely nothing'''
+        print("hello world")
+
+    def math_support(self):
+        '''
+        First test of the math module for sphinx
+        .. math:
+        a = b + c
+        '''
+        print("in the math support")
+
