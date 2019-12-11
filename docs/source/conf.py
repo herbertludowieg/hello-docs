99e30334838a571118573ebfb083d60f2610b328
Herbert Ludowieg <herbertl@buffalo.edu>
2019-12-11 10:20:32 -0500

    Change in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'hello-docs')))
 
 
 # -- Project information -----------------------------------------------------
@@ -28,6 +28,10 @@ author = 'Someone'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "nbsphinx"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -37,6 +41,8 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+
+source_suffix = [".rst", ".txt"]
 
 
 # -- Options for HTML output -------------------------------------------------
